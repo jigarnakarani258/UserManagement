c0ecaa940792c24244ddb8b327c74b7fa9ecc1ab
jigarnakarani258 <nakaranijigar258@gmail.com>
2022-09-18 19:32:44 +0530

    3) GetCurrentProfile and UpdateProfile done

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -2,6 +2,7 @@ const { Users } = require('./../Models/userModel')
 const { catchAsync } = require('./../Utility/catchAsync')
 const { AppError } = require('../Utility/appError')
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 // set up multer for storing uploaded files
 const multer = require('multer');
@@ -13,50 +14,59 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage }).single('photo');
 
-const signToken = (id) =>{
-    return jwt.sign({ id: id }, process.env.JWT_SECRETKEY, {
+const signToken = (id,email) =>{
+    return jwt.sign({ id: id, email: email }, process.env.JWT_SECRETKEY, {
         expiresIn: process.env.JWT_EXPIRESIN,
       });
 }
 
 const SignUp = catchAsync(async (req, res, next) => {
   upload(req, res, (err) => {
-    
-      const user = new Users({
-        name: req.body.name,
-        age: req.body.age,
-        gender: req.body.gender,
-        email: req.body.email,
-        password: req.body.password,
-        city: req.body.city,
-        state: req.body.state,
-        hobbies: req.body.hobbies,
-        Confirmpassword: req.body.Confirmpassword,
-        photo: {
-          data: req.file.filename,
-          contentType: "image/png",
-        },
-      });
-      user
-        .save()
-        .then(() => {
-          res.status(201).json({
-            status: "success",
-            data: {
-              newUser: {
-                id : user._id,
-                email : user.email,
-                name : user.name 
+      if(err){
+        return next(
+          new AppError(err.message, 400)
+        );
+      }
+      else
+      {
+        const user = new Users({
+          name: req.body.name,
+          age: req.body.age,
+          gender: req.body.gender,
+          email: req.body.email,
+          password: req.body.password,
+          city: req.body.city,
+          state: req.body.state,
+          hobbies: req.body.hobbies,
+          Confirmpassword: req.body.Confirmpassword,
+          photo: {
+            data: req.file.filename,
+            contentType: "image/png",
+          },
+        });
+        user
+          .save()
+          .then(() => {
+            res.status(201).json({
+              status: "success",
+              requestAt: req.requestTime,
+              data: {
+                newUser: {
+                  id : user._id,
+                  email : user.email,
+                  name : user.name
+                },
               },
-            },
-            message: "User SignUp Successfully!!",
+              message: "User SignUp Successfully!!",
+            });
+          })
+          .catch(err => {
+            return next(
+              new AppError(err.message, 400)
+            );
           });
-        })
-        .catch(err => {
-          return next(
-            new AppError(err.message, 400)
-          );
-        });   
+      }
+         
   });
 });
 
@@ -84,9 +94,10 @@ const SignIn = catchAsync(async (req, res, next) => {
     SignInUser.password
   );
   if (CorrectPassword) {
-    const token = signToken(SignInUser._id);
+    const token = signToken(SignInUser._id , SignInUser.email);
     res.status(200).json({
       status: 'Success',
+      requestAt: req.requestTime,
       token: `Bearer ${token}`,
       message: 'User SignIn successfully!!',
     });
@@ -98,7 +109,86 @@ const SignIn = catchAsync(async (req, res, next) => {
 
 });
 
+const getCurrentUser = catchAsync(async (req, res, next) => {  
+  const id = req.user._id
+  let currentUser= await Users.findById(id)
+  return res.status(200).send({
+      status: "success",
+      requestAt: req.requestTime,
+      user: currentUser
+    });
+});
+
+const updateCurrentUserProfile = catchAsync(async (req, res, next) => { 
+  upload(req, res,async (err) => {
+      if(err){
+        return next(
+          new AppError(err.message, 400)
+        );
+      }
+      else
+      {
+          const id = req.user._id
+          let updatedata ;
+          if(req.body.password){
+            req.body.password = await bcrypt.hash(req.body.password , 12);
+          }
+          if(req.file){
+            updatedata = {
+              name: req.body.name,
+              age: req.body.age,
+              gender: req.body.gender,
+              email: req.body.email,
+              password: req.body.password,
+              city: req.body.city,
+              state: req.body.state,
+              hobbies: req.body.hobbies,
+              Confirmpassword: req.body.Confirmpassword,
+              photo: {
+                data: req.file.filename,
+                contentType: "image/png",
+              },
+            }
+          }
+          else{
+            updatedata = {
+              name: req.body.name,
+              age: req.body.age,
+              gender: req.body.gender,
+              email: req.body.email,
+              city: req.body.city,
+              password: req.body.password,
+              state: req.body.state,
+              hobbies: req.body.hobbies,
+              Confirmpassword: req.body.Confirmpassword
+            };
+          }
+
+          let updatedUser = await  Users.findByIdAndUpdate(id,updatedata,{
+              new: true,
+              runValidators: true,
+          });
+          if(updatedUser === undefined || updatedUser === "" || updatedUser === null ) 
+          {
+               return next(new AppError("No user found with that id", 404));
+          } 
+          else {
+            return res.status(200).json({
+              status: "Success",
+              requestAt: req.requestTime,
+              message: `User profile updated successfully!!`,
+              updatedUser: updatedUser,
+            });
+          }
+      }         
+  }); 
+  
+});
+
+
 module.exports = { 
     SignUp,
-    SignIn
+    SignIn,
+    getCurrentUser,
+    updateCurrentUserProfile
 }
--- Utility/passport.js
+++ Utility/passport.js
@@ -5,12 +5,12 @@ var JwtStrategy = require('passport-jwt').Strategy,
     ExtractJwt = require('passport-jwt').ExtractJwt;
 var opts = {}
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = 'my-super-secret-key-for-project';
+opts.secretOrKey = process.env.JWT_SECRETKEY;
 
 
 passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
     
-    Users.findOne({id: jwt_payload.id}, function(err, user) {
+    Users.findOne({id: jwt_payload.id , email:jwt_payload.email}, function(err, user) {
         if (err) {
             return done(err, false);
         }
@@ -18,7 +18,6 @@ passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
             return done(null, user);
         } else {
             return done(null, false);
-            // or you could create a new account
         }
     });
 }));
